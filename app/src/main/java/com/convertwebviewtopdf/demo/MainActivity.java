b555c1f8eec613c23eccf4623137b3f5f90c7903
pramodkr <pramod@bookkeeperapp.net>
2018-04-06 10:35:38 +0530

    First commit

---
+++ app/src/main/java/com/convertwebviewtopdf/demo/MainActivity.java
@@ -0,0 +1,58 @@
+package com.convertwebviewtopdf.demo;
+
+import android.os.Build;
+import android.os.Environment;
+import android.print.PdfPrint;
+import android.print.PrintAttributes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    WebView webView;
+    Button button_convert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView=findViewById(R.id.webView);
+        button_convert=findViewById(R.id.button_convert);
+
+        webView.loadUrl("http://www.annalytics.co.uk/android/pdf/2017/04/06/Save-PDF-From-An-Android-WebView/");
+
+
+        button_convert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createWebPrintJob(webView);
+            }
+        });
+
+    }
+
+
+
+    private void createWebPrintJob(WebView webView) {
+        String jobName = getString(R.string.app_name) + " Document";
+        PrintAttributes attributes = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            attributes = new PrintAttributes.Builder()
+                    .setMediaSize(PrintAttributes.MediaSize.ISO_A4)
+                    .setResolution(new PrintAttributes.Resolution("pdf", "pdf", 600, 600))
+                    .setMinMargins(PrintAttributes.Margins.NO_MARGINS).build();
+        }
+        File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM + "/PDFTest/");
+        PdfPrint pdfPrint = new PdfPrint(attributes);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            pdfPrint.print(webView.createPrintDocumentAdapter(jobName), path, "output_" + System.currentTimeMillis() + ".pdf");
+        }
+    }
+}
